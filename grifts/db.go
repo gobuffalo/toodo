786f04b35b00c32dbee2de1d2cfd9680cc254296
Alan Scherger <ascherger@homeaway.com>
2018-03-02 01:09:16 -0600

    fix: DRY up model seeding

--- grifts/db.go
+++ grifts/db.go
@@ -16,44 +16,40 @@ var _ = grift.Namespace("db", func() {
 			if err := tx.TruncateAll(); err != nil {
 				return errors.WithStack(err)
 			}
-			u := &models.User{
+
+			u := models.User{
 				Email:                "mark@example.com",
 				Password:             "password",
 				PasswordConfirmation: "password",
 			}
 
-			if _, err := u.Create(tx); err != nil {
-				return errors.WithStack(err)
+			if err := tx.Create(&u); err != nil {
+				return err
 			}
 
-			item := &models.Item{
-				Title:  "Buy Milk",
-				UserID: u.ID,
+			items := []models.Item{
+				models.Item{
+					Title:  "Buy Milk",
+					UserID: u.ID,
+				},
+				models.Item{
+					Title:  "Learn Go",
+					Body:   nulls.NewString("What a great language"),
+					UserID: u.ID,
+				},
+				models.Item{
+					Title:     "Learn Buffalo",
+					UserID:    u.ID,
+					Completed: true,
+				},
 			}
 
-			if err := tx.Create(item); err != nil {
-				return errors.WithStack(err)
+			for _, i := range items {
+				if err := tx.Create(&i); err != nil {
+					return err
+				}
 			}
 
-			item = &models.Item{
-				Title:  "Learn Go",
-				Body:   nulls.NewString("What a great language"),
-				UserID: u.ID,
-			}
-
-			if err := tx.Create(item); err != nil {
-				return errors.WithStack(err)
-			}
-
-			item = &models.Item{
-				Title:     "Learn Buffalo",
-				UserID:    u.ID,
-				Completed: true,
-			}
-
-			if err := tx.Create(item); err != nil {
-				return errors.WithStack(err)
-			}
 			return nil
 		})
 	})
